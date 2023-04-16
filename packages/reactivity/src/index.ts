a889d18721652420723920b73c797eb2a2183ede
lihuan <1968763315@qq.com>
2023-04-16 15:06:15 +0800

    feat: 🎸 init

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: lihuan
+ * @Date: 2023-04-16 13:38:16
+ * @LastEditors: lihuan
+ * @LastEditTime: 2023-04-16 15:00:57
+ * @Email: 17719495105@163.com
+ */
+import { isObject } from '@lhvue/shared'
+console.log(isObject(21))
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: lihuan
+ * @Date: 2023-04-16 13:38:20
+ * @LastEditors: lihuan
+ * @LastEditTime: 2023-04-16 14:41:33
+ * @Email: 17719495105@163.com
+ */
+export const isObject = (val) => val !== null && typeof val === 'object'
