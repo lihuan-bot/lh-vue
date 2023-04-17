a8a24b1dc12b7683e6243ef7493fa13561cc77a8
lihuan <1968763315@qq.com>
2023-04-17 11:30:23 +0800

    feat: 🎸 添加husky

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -2,8 +2,8 @@
  * @Author: lihuan
  * @Date: 2023-04-16 13:38:16
  * @LastEditors: lihuan
- * @LastEditTime: 2023-04-17 10:40:35
+ * @LastEditTime: 2023-04-17 11:28:56
  * @Email: 17719495105@163.com
  */
 import { isObject } from '@lhvue/shared'
-console.log(isObject(2))
+console.log(isObject('2'))
